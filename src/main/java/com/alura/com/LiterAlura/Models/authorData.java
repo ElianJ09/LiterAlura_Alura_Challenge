601634168766078f58783c17c0b609aa5e0ba50e
ElianRG09 <kutakikato80@gmail.com>
2024-07-14 15:55:37 -0600

    rename files

--- src/main/java/com/alura/com/LiterAlura/Models/authorData.java
+++ src/main/java/com/alura/com/LiterAlura/Models/authorData.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record authorInfo(
+public record authorData(
         @JsonAlias("name") String name,
         @JsonAlias("birth_year") int dateOfBirth,
         @JsonAlias("death_year") int dateOfDeath) {
--- src/main/java/com/alura/com/LiterAlura/Models/bookData.java
+++ src/main/java/com/alura/com/LiterAlura/Models/bookData.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record bookInfo(
+public record bookData(
         @JsonAlias("title") String title,
         @JsonAlias("name") Author author,
         @JsonAlias("languages") List<String> language,
